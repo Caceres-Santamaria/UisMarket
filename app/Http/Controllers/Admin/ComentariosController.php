5b006902814ca5f93d416f79a401c78ab9994543
jmsr06 <jennysantamaria06@gmail.com>
2022-01-18 21:29:49 -0500

    modulo administrador

---
+++ app/Http/Controllers/Admin/ComentariosController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tienda;
+use Illuminate\Http\Request;
+
+class ComentariosController extends Controller
+{
+  public function index(Tienda $tienda){
+
+    return view('admin.comentarios', compact('tienda'));
+  }
+}
--- app/Http/Controllers/Admin/TiendasController.php
+++ app/Http/Controllers/Admin/TiendasController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 
 class TiendasController extends Controller
 {
+  public $modal = false;
+  public $tienda = null;
   public function index()
   {
     return view('admin.tiendas');
