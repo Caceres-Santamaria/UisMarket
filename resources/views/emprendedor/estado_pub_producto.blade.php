1ecdff3a12b0df1a371a0fa96a23f19138a90fed
jmsr06 <jennysantamaria06@gmail.com>
2022-01-21 19:28:43 -0500

    vistas prototipo 4

--- resources/views/emprendedor/estado_pub_producto.blade.php
+++ resources/views/emprendedor/estado_pub_producto.blade.php
@@ -1,8 +1,10 @@
-
+@if ($producto->publicacion == 1)
+    
 <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
   Borrador
 </span>
-
-<span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+@else
+    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
   Publicado
 </span>
+@endif
