22127b3094573b5d43fb0cb36c4ad6afd7a77db9
Alexis3004 <nelsonalexiscacerzcarreo37@gmail.com>
2022-06-05 20:27:17 -0500

    nav home, calificación pedido, cantidad carrito

--- app/Http/Livewire/CalificacionPedido.php
+++ app/Http/Livewire/CalificacionPedido.php
@@ -3,14 +3,16 @@
 namespace App\Http\Livewire;
 
 use App\Models\Calificacion;
+use App\Models\Pedido;
 use Livewire\Component;
 
 class CalificacionPedido extends Component
 {
     protected $listeners = ['render'];
     public $modal = false;
-    public $pedido_id;
+    public Pedido $pedido;
     public Calificacion $calificacion;
+    public $contenido;
 
     protected function rules()
     {
@@ -21,22 +23,28 @@ class CalificacionPedido extends Component
         ];
     }
 
-    public function mount(Calificacion $calificacion, $id){
-        $this->calificacion = $calificacion;
-        $this->calificacion->pedido_id = $id;
-        $this->pedido_id = $id;
+    public function mount(Pedido $pedido) {
+        $this->calificacion = $pedido->calificacion ? $pedido->calificacion : new Calificacion();
+        if(!$this->calificacion->id) {
+            $this->calificacion->pedido_id = $pedido->id;
+            $this->contenido = '';
+        } else {
+            $this->contenido = $this->calificacion->contenido;
+        }
+        $this->pedido = $pedido;
     }
 
-    public function updated($item){
-        $this->validateOnly($item);
-    }
+    // public function updated($item){
+    //     $this->validateOnly($item);
+    // }
 
     public function guardar(){
-        $this->calificacion->contenido = $this->calificacion->contenido == "" ? null : $this->calificacion->contenido;
-        $this->calificacion->pedido_id = $this->pedido_id;
+        $this->calificacion->contenido = trim($this->calificacion->contenido) == "" ? null : trim($this->calificacion->contenido);
+        // $this->calificacion->pedido_id = $this->pedido_id;
         $this->validate();
         $this->modal = false;
         $this->calificacion->save();
+        $this->contenido = $this->calificacion->contenido;
     }
 
     public function render()
@@ -47,11 +55,11 @@ class CalificacionPedido extends Component
     public function cancelar(){
         $this->modal = false;
         if($this->calificacion->id){
-            $this->calificacion = Calificacion::where('pedido_id',$this->pedido_id)->first();
-            // return redirect()->route('pedidos.show',$this->pedido_id);
+            $this->calificacion = $this->pedido->calificacion;
         }
         else{
-            $this->calificacion = new Calificacion();
+            $this->calificacion->contenido = null;
+            $this->calificacion->calificacion = null;
         }
     }
 }
